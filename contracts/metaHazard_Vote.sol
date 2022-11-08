afc36ad9b4d81eaae581216b3bae0da415eb8fac
201524554 <43158434+201524554@users.noreply.github.com>
2022-11-08 14:05:50 +0900

    code clean up

--- contracts/metaHazard_Vote.sol
+++ contracts/metaHazard_Vote.sol
@@ -7,11 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract metaH_Vote is ERC20, ERC20Permit, ERC20Votes {
 
-    address private _owner;
 
-    constructor() ERC20("metaH_Vote", "MH_V") ERC20Permit("metaH_Vote"){
-        _owner = msg.sender;
-        mint(_owner, 100);
+    constructor() ERC20("metaH_Vote", "metaH") ERC20Permit("metaH_Vote"){
+        _mint(msg.sender, 1000 * 1 ether);
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
@@ -26,23 +24,4 @@ contract metaH_Vote is ERC20, ERC20Permit, ERC20Votes {
         super._burn(from, amount);
     }
 
-    function mint(address to, uint amount) public {
-        require(msg.sender == _owner, "onlyOwner");
-        _mint(to, amount);
-    }
-
-    function burn(address from, uint amount) public {
-        require(msg.sender == _owner, "onlyOwner");
-        _burn(from, amount);
-    }
-
-    function delegate(address to) public override {
-        _delegate(msg.sender, to);
-    }
-
-    function transfer(address to, uint amount) public override returns(bool){
-        super.transfer(to, amount);
-        _afterTokenTransfer(msg.sender, to, amount);
-        return true;
-    }
 }
